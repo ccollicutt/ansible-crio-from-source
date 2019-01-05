10a73bec7b5f73cf0b2e7d9896e755d64e79fba0
curtis <curtis@serverascode.com>
2019-01-05 12:09:16 -0500

    rename test to crio

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,7 +6,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_nginx_running_and_enabled(host):
+def test_crio_running_and_enabled(host):
     crio = host.service("crio")
     assert crio.is_running
     assert crio.is_enabled
